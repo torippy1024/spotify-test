17afe32e1ae867a28a802c60272611ca6fab23f8
h.toriyama <toriyamahiroto1128@gmail.com>
2022-10-15 15:50:29 +0900

    トップページのm/p設定

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -6,7 +6,7 @@ const TopPage = () => {
   return  (
       <div data-theme="light" className="flex flex-col min-h-screen">
       <Header />
-      <div className="grow container mx-auto my-5">
+      <div className="grow container mx-auto my-4 px-4">
         TopPage
       </div>
       <Footer />

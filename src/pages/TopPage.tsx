d6977a8876e902d23c7b32a04ca9c5ea7c8762d0
h.toriyama <toriyamahiroto1128@gmail.com>
2022-12-17 05:43:06 +0900

    array&mapで渡してみる

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -4,11 +4,24 @@ import Header from "../components/Header";
 import SpotifyEmbedded from "../components/SpotifyEmbedded";
 
 const TopPage = () => {
+  const trackIds = [
+    "2uOu92n9vW1yHir6Na8vpM",
+    "1lAz5N2RaBtHmGapbnLwm4",
+    "2uOu92n9vW1yHir6Na8vpM",
+    "1lAz5N2RaBtHmGapbnLwm4",
+    "2uOu92n9vW1yHir6Na8vpM",
+    "1lAz5N2RaBtHmGapbnLwm4",
+  ];
+
   return  (
       <div data-theme="dark" className="flex flex-col min-h-screen">
       <Header />
       <div className="grow container mx-auto my-4 px-4">
-        <SpotifyEmbedded id="2uOu92n9vW1yHir6Na8vpM" />
+        {trackIds.map((id, index) =>
+          <div key={index} className="mb-4">
+            <SpotifyEmbedded id={id} />
+          </div>
+        )}
       </div>
       <Footer />
     </div>

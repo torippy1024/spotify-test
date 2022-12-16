d26cd46037cec7544e836a41a43469aa035ee172
h.toriyama <toriyamahiroto1128@gmail.com>
2022-12-17 05:32:21 +0900

    埋め込みをコンポーネント化

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import SpotifyEmbedded from "../components/SpotifyEmbedded";
 
 const TopPage = () => {
   return  (
       <div data-theme="light" className="flex flex-col min-h-screen">
       <Header />
       <div className="grow container mx-auto my-4 px-4">
-        <iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/track/2uOu92n9vW1yHir6Na8vpM?utm_source=generator" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <SpotifyEmbedded id="2uOu92n9vW1yHir6Na8vpM" darkTheme={true} />
       </div>
       <Footer />
     </div>
